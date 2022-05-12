ed275b22fe20090300df17acfa78c45f85c06632
Simon Rofe <simon.rofe@ieee.org>
2022-05-12 20:24:16 +1000

    Initial commit of new macOS project

---
+++ OnThisDay/OnThisDayApp.swift
@@ -0,0 +1,18 @@
+//
+//  OnThisDayApp.swift
+//  OnThisDay
+//
+//  Created by Simon Rofe on 12/5/2022.
+//
+//
+
+import SwiftUI
+
+@main
+struct OnThisDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
