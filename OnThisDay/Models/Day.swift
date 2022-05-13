a8457442799b99d16e307bd5c71abf425678c0e7
Simon Rofe <simon.rofe@ieee.org>
2022-05-13 11:02:59 +1000

    Add view model - AppState
    Fix some minor typographical issues to allow project to be built.

--- OnThisDay/Models/Day.swift
+++ OnThisDay/Models/Day.swift
@@ -8,9 +8,9 @@ struct Day: Decodable {
     let date: String
     let data: [String:[Event]]
 
-    var events: [Event] { data[EventType.events.rawvalue] ?? [] }
-    var births: [Event] { data[EventType.births.rawvalue] ?? [] }
-    var deaths: [Event] { data[EventType.deaths.rawvalue] ?? [] }
+    var events: [Event] { data[EventType.events.rawValue] ?? [] }
+    var births: [Event] { data[EventType.births.rawValue] ?? [] }
+    var deaths: [Event] { data[EventType.deaths.rawValue] ?? [] }
 
     var displayDate: String {
         date.replacingOccurrences(of: "_", with: " ")
