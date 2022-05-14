7a86834667aac790a3691ecfeb0cfe3713264c38
Simon Rofe <simon.rofe@ieee.org>
2022-05-14 13:51:16 +1000

    Add custom menu item to Help menu
    This opens the website for Zen Quotes in the default web browser. It
    also defines the keyboard shortct Command-/ to also perform this
    action.

--- OnThisDay/Menus.swift
+++ OnThisDay/Menus.swift
@@ -11,5 +11,20 @@ struct Menus: Commands {
     var body: some Commands {
         SidebarCommands()
         ToolbarCommands()
+
+        CommandGroup(before: .help) {
+            Button("ZenQuotes.io web site") {
+                showAPIWebSite()
+            }
+            .keyboardShortcut("/", modifiers: .command)
+        }
+    }
+
+    private func showAPIWebSite() {
+        let address = "https://today.zenquotes.io"
+        guard let url = URL(string: address) else {
+            fatalError("Invalid address")
+        }
+        NSWorkspace.shared.open(url)
     }
 }
