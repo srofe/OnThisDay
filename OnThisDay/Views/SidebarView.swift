1b5b2dd4a1dc31d2b5b39f5ab8b9d62edbad4d21
Simon Rofe <simon.rofe@ieee.org>
2022-05-16 08:14:46 +1000

    Add list of selected dates to sidebar

--- OnThisDay/Views/SidebarView.swift
+++ OnThisDay/Views/SidebarView.swift
@@ -7,6 +7,7 @@ import SwiftUI
 struct SidebarView: View {
     @EnvironmentObject var appState: AppState
     @AppStorage("showTotals") var showTotals = true
+    @SceneStorage("selectedDate") var selectedDate: String?
     @Binding var selection: EventType?
 
     var body: some View {
@@ -16,6 +17,19 @@ struct SidebarView: View {
                     ForEach(EventType.allCases, id: \.self) { type in
                         Text(type.rawValue)
                             .badge(showTotals ? appState.countFor(eventType: type) : 0)
+                    }
+                }
+                Section("AVAILABLE DATES") {
+                    ForEach(appState.sortedDates, id: \.self) { date in
+                        Button {
+                            selectedDate = date
+                        } label: {
+                            HStack {
+                                Text(date)
+                                Spacer()
+                            }
+                        }
+                        .controlSize(.large)
                     }
                 }
             }
