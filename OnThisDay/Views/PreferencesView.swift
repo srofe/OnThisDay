fa452182980b2b886c1776705f9143ab76e14abd
Simon Rofe <simon.rofe@ieee.org>
2022-05-16 20:20:25 +1000

    Add a preferences view and set it's title
    The view is also set to a fixed size using the frame modifier.

---
+++ OnThisDay/Views/PreferencesView.swift
@@ -0,0 +1,12 @@
+//
+// Created by Simon Rofe on 16/5/2022.
+//
+
+import SwiftUI
+
+struct PreferencesView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(width: 200, height: 150)
+    }
+}
